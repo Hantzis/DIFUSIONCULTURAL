fbac3935a3a8d777ee4a9f98e16af3fe4f499176
lm <luismguzmanz@gmail.com>
2019-09-02 11:01:25 -0500

    Default Changelist

--- difusion_cultural/models.py
+++ difusion_cultural/models.py
@@ -14,7 +14,7 @@ from wagtail.search import index
 from wagtail.snippets.models import register_snippet
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from modelcluster.contrib.taggit import ClusterTaggableManager
-from taggit.models import TaggedItemBase, Tag as TaggitTag
+from taggit.models import TaggedItemBase, TagBase, Tag as TaggitTag
 from base.blocks import ExtraStreamBlock
 from wagtail.snippets.models import register_snippet
 
@@ -31,7 +31,7 @@ register_snippet(Collection)
 
 class DifusionCulturalHomePage(HomePage):
     def get_nuevos_bisnietos(self):
-        return Page.objects.filter(Q(depth__gte=5))[:3]
+        return Page.objects.filter(Q(depth__gte=5))[:6]
 
 
 
@@ -101,15 +101,12 @@ class DifusionCulturalDependencia(Page):
     subpage_types = ['DifusionCulturalNoticia']
     parent_page_types = ['DifusionCulturalCartelera']
 
-
-class DifusionCulturalNoticiaEtiqueta(TaggedItemBase):
-    """
-    This model allows us to create a many-to-many relationship between
-    the BlogPage object and tags. There's a longer guide on using it at
-    http://docs.wagtail.io/en/latest/reference/pages/model_recipes.html#tagging
-    """
-    content_object = ParentalKey('DifusionCulturalNoticia', related_name='yeko_garage_tagged_items',
-                                 on_delete=models.CASCADE)
+'''
+@register_snippet
+class DifusionCulturalEtiqueta(TaggitTag):
+    class Meta:
+        proxy = True
+'''
 
 
 class DifusionCulturalNoticiaQuerySet(PageQuerySet):
@@ -146,7 +143,7 @@ class DifusionCulturalNoticia(Page):
         on_delete=models.SET_NULL,
         help_text='Select the image collection for this gallery.'
     )
-    etiquetas = ClusterTaggableManager(through=DifusionCulturalNoticiaEtiqueta, blank=True)
+    # etiquetas = ClusterTaggableManager(through=DifusionCulturalEtiqueta, blank=True)
 
     search_fields = Page.search_fields + [
         index.SearchField('introduccion'),
@@ -159,6 +156,7 @@ class DifusionCulturalNoticia(Page):
         StreamFieldPanel('cuerpo'),
         ImageChooserPanel('imagen'),
         FieldPanel('galeria'),
+        #FieldPanel('etiquetas'),
     ]
 
     objects = DifusionCulturalNoticiaManager()
