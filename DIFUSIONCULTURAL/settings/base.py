a6bfb8d302ab12a415e9db4e1e47047e3e103d40
lm <luismguzmanz@gmail.com>
2019-09-10 07:46:23 -0500

    Maintenance mode login - misc css

--- DIFUSIONCULTURAL/settings/base.py
+++ DIFUSIONCULTURAL/settings/base.py
@@ -176,3 +176,6 @@ WAGTAIL_SITE_NAME = "DIFUSIONCULTURAL"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+#TODELETE on final deploy
+PASSWORD_REQUIRED_TEMPLATE = 'maintenance_login.html'
