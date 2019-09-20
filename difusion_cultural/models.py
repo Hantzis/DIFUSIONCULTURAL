e23ecdb40541fd5b6c741e768974818c73064069
lm <luismguzmanz@gmail.com>
2019-09-19 21:36:12 -0500

    Add model page types: Blog, Nota, Articulo; Add fields to model Noticia: fecha, horarios, lugar, , etc..

--- difusion_cultural/models.py
+++ difusion_cultural/models.py
@@ -44,14 +44,97 @@ class DifusionCulturalHomePage(HomePage):
 
 
 
-# class DifusionCulturalArticulo(StandardPage):
-#     subpage_types = []
-#     parent_page_types = ['DifusionCulturalHomePage']
-#
-#
-#     class Meta:
-#         verbose_name = "Artículo"
-#         verbose_name_plural = "Artículos"
+class DifusionCulturalBlog(Page):
+    subpage_types = ['DifusionCulturalArticulo']
+    parent_page_types = ['DifusionCulturalHomePage']
+
+
+    def get_nietos(self):
+        return Page.objects.descendant_of(self, inclusive=False).not_child_of(self)
+
+    def get_nuevos_nietos(self):
+        return Page.objects.descendant_of(self, inclusive=False).not_child_of(self)[:2]
+
+
+    @classmethod
+    def can_create_at(cls, parent):
+        # You can only create one of these!
+        return super(DifusionCulturalBlog, cls).can_create_at(parent) \
+               and not cls.objects.exists()
+
+    class Meta:
+        verbose_name = "Blog"
+        verbose_name_plural = "Blog"
+
+
+
+
+class DifusionCulturalArticulo(StandardPage):
+    fecha = models.DateField("Fecha de publicación")
+    autor = models.CharField(max_length=250)
+    introduccion = models.CharField(max_length=250)
+    cuerpo = StreamField(
+        ExtraStreamBlock(), verbose_name="Page body", blank=True
+    )
+    imagen = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Imagen de portada'
+    )
+    # acotaciones = models.CharField(verbose_name="Acotaciones", max_length=250)
+    galeria = models.ForeignKey(
+        Collection,
+        limit_choices_to=~models.Q(name__in=['Root']),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        help_text='Select the image collection for this gallery.'
+    )
+    #etiquetas = ClusterTaggableManager(through='DifusionCulturalNoticiaEtiqueta', blank=True)#viene de noticia: TODO: adaptar a ArticuloDeBlog
+
+    search_fields = Page.search_fields + [
+        index.SearchField('introduccion'),
+        index.SearchField('cuerpo'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('fecha'),
+        FieldPanel('introduccion'),
+        FieldPanel('autor'),
+        StreamFieldPanel('cuerpo'),
+        ImageChooserPanel('imagen'),
+        FieldPanel('galeria'),
+    ]
+
+    subpage_types = []
+    parent_page_types = ['DifusionCulturalBlog']
+
+    class Meta:
+        verbose_name = "Artículo"
+        verbose_name_plural = "Artículos"
+
+
+class DifusionCulturalNota(Page):
+
+    # Body section of the HomePage
+    body = StreamField(
+        ExtraStreamBlock(), verbose_name="Home content block", blank=True
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
+    subpage_types = []
+    parent_page_types = ['DifusionCulturalHomePage','DifusionCulturalPagina','DifusionCulturalNota']
+
+    class Meta:
+        verbose_name = "Nota"
+        verbose_name_plural = "Notas"
+
 
 
 class DifusionCulturalCartelera(Page):
@@ -176,6 +259,10 @@ class Tag(TaggitTag):
 class DifusionCulturalNoticia(Page):
     fecha = models.DateField("Fecha de publicación")
     introduccion = models.CharField(max_length=250)
+    fecha_de_evento = models.CharField(verbose_name="Fecha_de_evento", max_length=250, null=True)
+    horarios = models.CharField(verbose_name="Horarios", max_length=250, null=True)
+    lugar = models.CharField(verbose_name="Lugar", max_length=250, null=True)
+    consideraciones = models.CharField(verbose_name="Consideraciones", max_length=250, null=True)
     cuerpo = StreamField(
         ExtraStreamBlock(), verbose_name="Page body", blank=True
     )
@@ -205,6 +292,10 @@ class DifusionCulturalNoticia(Page):
     content_panels = Page.content_panels + [
         FieldPanel('fecha'),
         FieldPanel('introduccion'),
+        FieldPanel('fecha_de_evento'),
+        FieldPanel('horarios'),
+        FieldPanel('lugar'),
+        FieldPanel('consideraciones'),
         StreamFieldPanel('cuerpo'),
         ImageChooserPanel('imagen'),
         FieldPanel('galeria'),
